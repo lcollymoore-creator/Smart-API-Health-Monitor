4afe14b4015a2918ce21171004440b440da3cfd2
Luis Rafael collymoore <lcollymoore@gmail.com>
2026-03-31 17:59:44 -0400

    request structure created

---
+++ health-monitor/core/httpClient.ts
@@ -0,0 +1,39 @@
+import { request } from '@playwright/test';
+
+
+export async function sendRequest({
+  baseURL = '',
+  endpoint = '',
+  method = '',
+  headers = {},
+  data = null
+}) {
+  const apiContext = await request.newContext({
+    baseURL,
+    extraHTTPHeaders: headers,
+  });
+
+  let response;
+
+  switch (method.toUpperCase()) {
+    case 'POST':
+      response = await apiContext.post(endpoint, { data });
+      break;
+    case 'PUT':
+      response = await apiContext.put(endpoint, { data });
+      break;
+    case 'DELETE':
+      response = await apiContext.delete(endpoint);
+      break;
+    default:
+      response = await apiContext.get(endpoint);
+  }
+
+  const responseBody = await response.json().catch(() => null);
+
+  return {
+    status: response.status(),
+    headers: response.headers(),
+    body: responseBody,
+  };
+}
--- health-monitor/core/runner.spec.ts
+++ health-monitor/core/runner.spec.ts
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test';
+import { services } from '../config/services.json';
+import { sendRequest } from './httpClient.ts';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('validar servicios desde JSON', async () => {
+  for (const service of services) {
+    const response = await sendRequest({
+      baseURL: service.url,
+      endpoint: service.endpoint,
+      method: service.method,
+    });
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+ 
+    expect(response.status).toBe(service.expectedStatus);
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  }
 });
